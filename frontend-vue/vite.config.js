f1db786064b63005b19d25890fb4f25c08fbb192
David Sousa <davidlsousa.dev@gmail.com>
2025-03-30 15:54:03 -0300

    config para subir em 127.0.0.1

--- frontend-vue/vite.config.js
+++ frontend-vue/vite.config.js
@@ -7,6 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 3002,
+  },
   plugins: [
     vue(),
     vueDevTools(),
